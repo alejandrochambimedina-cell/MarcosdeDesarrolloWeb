e62f62e467add152bf3cbc0fe9e7986a619fbb35
LAB-USR-ATE <LAB-USR-ATE@grupoutp.net>
2026-09-23 19:51:57 -0500

    Guia 5

---
+++ Laboratorio 5/johan_arango/techlab-web/src/test/java/johan_arango/techlab_web/TechlabWebApplicationTests.java
@@ -0,0 +1,13 @@
+package johan_arango.techlab_web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechlabWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
